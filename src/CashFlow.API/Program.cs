c127fa0a521dc04e7cad1a34e6d6f1ce793f47dd
SAMUEL MOTA <samuelhpmota@gmail.com>
2025-05-31 09:47:36 -0300

    implementando filtro de exceptions

--- src/CashFlow.API/Program.cs
+++ src/CashFlow.API/Program.cs
@@ -1,15 +1,18 @@
+using CashFlow.API.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//configura o filtro para que a API execute-o
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();

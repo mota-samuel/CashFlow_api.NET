adf8d98e714eda65a798ba81cca06626ee7eb124
SAMUEL MOTA <samuelhpmota@gmail.com>
2025-06-04 12:04:17 -0300

    First test - test of validation

---
+++ test/ValidationTestes/Expenses/Register/RegisterExpensesValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace ValidationTestes.Expenses.Register;
+public class RegisterExpensesValidatorTests
+{
+    [Fact]
+    public void success()
+    {
+        //arrenge
+        var validator = new RegisterExpensesValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Title = "Test",
+            Amount = 100,
+            Description = "Test description",
+            Date = DateTime.Now.AddDays(-2),
+            Type = CashFlow.Communication.Enums.PaymentType.EletronicTransfer
+        };
+        //act
+        var result = validator.Validate(request);
+
+       //assert
+       Assert.True(result.IsValid);
+    }
+}
+

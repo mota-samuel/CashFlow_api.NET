fe9f6171f4919c0b48fbf4f97ed1e6c2cf87f4c0
SAMUEL MOTA <samuelhpmota@gmail.com>
2025-06-06 10:23:25 -0300

    Creating fake validators wit Bogus

---
+++ test/CommonTestUtilities/Request/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Request;
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestRegisterExpenseJson Build()
+    {
+        return new Faker<RequestRegisterExpenseJson>()
+            .RuleFor(x => x.Title, f => f.Commerce.ProductName())
+            .RuleFor(x => x.Description, f => f.Lorem.Paragraph())
+            .RuleFor(x => x.Date, f => f.Date.Past(1))
+            .RuleFor(x => x.Amount, f => f.Finance.Amount(10, 1000, 2))
+            .RuleFor(x => x.Type, f => f.PickRandom<CashFlow.Communication.Enums.PaymentType>());
+  
+    }
+}
--- test/ValidationTestes/Expenses/Register/RegisterExpensesValidatorTests.cs
+++ test/ValidationTestes/Expenses/Register/RegisterExpensesValidatorTests.cs
@@ -1,22 +1,18 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CommonTestUtilities.Request;
 
 namespace ValidationTestes.Expenses.Register;
 public class RegisterExpensesValidatorTests
 {
+
     [Fact]
     public void success()
     {
         //arrenge
         var validator = new RegisterExpensesValidator();
-        var request = new RequestRegisterExpenseJson
-        {
-            Title = "Test",
-            Amount = 100,
-            Description = "Test description",
-            Date = DateTime.Now.AddDays(-2),
-            Type = CashFlow.Communication.Enums.PaymentType.EletronicTransfer
-        };
+        var request =  RequestRegisterExpenseJsonBuilder.Build();
+
         //act
         var result = validator.Validate(request);
 
